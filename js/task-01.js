d13d6f5c7a4cfd0ff7b04ad04b5e2eab529ecd86
OleksandrFilin <acfiln@ukr.net>
2023-07-27 12:23:12 +0300

    correct task1

--- js/task-01.js
+++ js/task-01.js
@@ -4,6 +4,8 @@ console.log("Number of categories:", arrLi.length);
 arrLi.forEach((itemLi) => {
   const headEl = itemLi.firstElementChild;
   console.log("Category: ", headEl.textContent);
-  const arrLi2 = itemLi.querySelectorAll("li");
-  console.log("Elements: ", arrLi2.length);
+  //const arrLi2 = itemLi.querySelectorAll("li");
+  //console.log("Elements: ", arrLi2.length);
+  const elm = itemLi.lastElementChild.children;
+  console.log("Elements:", elm.length);
 });
