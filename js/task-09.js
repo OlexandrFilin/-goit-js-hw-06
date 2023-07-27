26b8d1267a84dc872b1938764eb61c23939d20db
OleksandrFilin <acfiln@ukr.net>
2023-07-27 10:00:51 +0300

    run task 10

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnElm = document.querySelector(".change-color");
+const bodyElm = document.querySelector("body");
+const txtColorElm = document.querySelector("span[class = 'color']");
+
+btnElm.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  const colorRandom = getRandomHexColor();
+  bodyElm.style.backgroundColor = colorRandom;
+  txtColorElm.textContent = colorRandom;
+}
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inpAmountElm = document.querySelector("input[type = 'number']");
+inpAmountElm.value = 1;
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+
+const divBox = document.querySelector("#boxes");
+function greatrBox(ammount) {
+  let size = 30;
+  let colorRandom = getRandomHexColor();
+  let block = "";
+  for (let i = 1; i <= ammount; i += 1) {
+    block += `<div style= 'background-color:${colorRandom}; width:${size}px;height:${size}px; margin-bottom :10px;'></div>`;
+    colorRandom = getRandomHexColor();
+    size += 10;
+  }
+  return block;
+}
+
+btnCreate.addEventListener("click", onClickBtnCreateBox);
+function onClickBtnCreateBox() {
+  divBox.innerHTML = "";
+  divBox.insertAdjacentHTML("beforeend", greatrBox(inpAmountElm.value));
+}
+btnDestroy.addEventListener("click", onClickBtnDestroy);
+function onClickBtnDestroy() {
+  divBox.innerHTML = "";
+}
